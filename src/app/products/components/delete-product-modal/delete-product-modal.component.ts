0d1e1f818435789b13b445b6bc5c4d3f808d08a2
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-02 07:54:58 -0500

    Fixing: DeleteProductModal error

--- src/app/products/components/delete-product-modal/delete-product-modal.component.ts
+++ src/app/products/components/delete-product-modal/delete-product-modal.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'tm-delete-product-modal',
   imports: [],
-  templateUrl: './delete-modal.component.html',
+  templateUrl: './delete-product-modal.component.html',
 })
 export class DeleteModalComponent {}
