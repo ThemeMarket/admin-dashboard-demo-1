f40bcbc599a5b5053fd4ffca995da089a05dbdd8
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-16 07:19:41 -0500

    Base structure for sales page added

--- src/app/sales/sales.component.ts
+++ src/app/sales/sales.component.ts
@@ -1,8 +1,22 @@
-import { Component } from '@angular/core';
+import { OrderService } from '@/core/services/order.service';
+import { ListLoadingComponent } from '@/shared/components/list-loading/list-loading.component';
+import { Order } from '@/shared/models/order';
+import { CurrencyPipe } from '@angular/common';
+import { Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'tm-sales',
-  imports: [],
+  imports: [ListLoadingComponent, CurrencyPipe],
   templateUrl: './sales.component.html',
+  styleUrl: './sales.component.css',
 })
-export class SalesComponent {}
+export class SalesComponent implements OnInit {
+  private readonly orderService = inject(OrderService);
+  protected orders = signal<Order[]>([]);
+
+  ngOnInit(): void {
+    this.orderService.getAll().subscribe((orders) => {
+      this.orders.set(orders);
+    });
+  }
+}
