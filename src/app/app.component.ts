f1a982d33bd1d43b566f1412f7bd90a328643c43
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-18 06:59:08 -0500

    Angular build budget size changed to allow major bundle size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
-import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
-import { initFlowbite } from 'flowbite';
+import { NavbarComponent } from '@/shared/components/navbar/navbar.component';
+import { SidebarComponent } from '@/shared/components/sidebar/sidebar.component';
+import { loadFlowbiteComponents } from '@/shared/utils/load-flowbite-components';
 
 @Component({
   selector: 'app-root',
@@ -11,6 +11,6 @@ import { initFlowbite } from 'flowbite';
 })
 export class AppComponent implements OnInit {
   ngOnInit(): void {
-    initFlowbite();
+    loadFlowbiteComponents();
   }
 }
